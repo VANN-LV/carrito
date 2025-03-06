9c3ce2388909e8ed23f563fa93d15860a78a38f4
alumnog <alumnog@s921-36.itson.edu.mx>
2025-03-06 11:02:40 -0600

    Se agrego numerador para TipoMotor

--- src/mx/itson/carrito/ui/Main.java
+++ src/mx/itson/carrito/ui/Main.java
@@ -4,25 +4,41 @@
  */
 package mx.itson.carrito.ui;
 
+import java.util.Scanner;
+import mx.itson.carrito.entidades.Carro;
+import mx.itson.carrito.enums.TipoMotor;
+
 /**
  *
  * @author alumnog
  */
 public class Main {
-    
-    
-    carro bmw = new carro();
-    bmw.setmarca("BMW");
-    bmw.setcolor("blanco");
-    bmw.setAnio(2021);
-    bwn.getKilometraje(10000);
-    
-    double tiempo = bmw.calcularTiempo
-    
-    System.out.println.("El yiempo que le tomara al carro");
-    "de color"+   bmw.getColor() +
-    "cuyo modelo es " + bmw.getAnio();
-    " sera de" + tiempo + 
-    "y ahora su kilometraje es de " + bmw.getkilometraje
-            
+ public static void main(String[] args) {
+        
+        Scanner sc= new Scanner(System.in);
+        
+        System.out.println("Ingrese la distancia recorrida");
+        double distancia = sc.nextDouble();
+        
+        System.out.println("Ingrese la velocisad a la quye viaja el carro ");
+        double velocidad = sc.nextDouble();
+        
+        //Generamos una intsancia de la clase carro
+        Carro bmw= new Carro();
+        bmw.setModelo(" BMW");
+        bmw.setColor("negro");
+        bmw.setAnio(2022);
+        bmw.setKilometraje(10000);
+        bmw.setTipoMotor(TipoMotor.HÍBRIDO);
+        
+        double tiempo= bmw.calcularTiempo(distancia, velocidad);
+        
+        System.out.println("El tiempo que tomará al carro "
+                + bmw.getModelo()+
+                "de color" + bmw.getColor() +
+                "y cuyo modelo es" + bmw.getAnio()+
+                "y su kilometraje es de" + bmw.getKilometraje()
+                +tiempo
+        );
+ }
 }
